e44876a9c60e43374f6f4d81672d303560028662
junzzi <jjh01027717502@gmail.com>
2021-09-27 17:26:37 +0900

    feat: Edit webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,4 @@
-require('dotenv').config();
 const path = require('path');
-
 const isProd = process.env.NODE_ENV === 'production';
 
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -10,30 +8,21 @@ module.exports = {
   devtool: isProd ? 'hidden-source-map' : 'source-map', // development 환경에서만 source-map을 만든다. ->외부에서 리액트 구조를 보고싶을때
   entry: './src/index.tsx',
   output: {
-    filename: '[name].js', // [name]은 청크의 이름을 사용한다. ❓
-    path: path.join(__dirname, '/dist'), // ❓
+    filename: '[name].js', // [name]은 청크의 이름을 사용한다.
+    path: path.join(__dirname, '/dist'), //
   },
   // node_modules -> 외부 라이브러리를 바로 가져올 수 있다.
   // resolve는 웹팩이 알아서 경로 확장자를 처리할 수 있도록한다.
   resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src/'),
+    },
     modules: ['node_modules'],
-    extensions: ['.js', '.jsx', '.ts', '.tsx'], // ❓
-    '@': path.resolve(__dirname, 'src/'),
+    extensions: ['.js', '.jsx', '.ts', '.tsx'], //
   },
 
   module: {
     rules: [
-      {
-        test: /\.(ts|js)x?$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            // typescript 자체적으로 트랜스파일링 기능이 있기 때문에 바벨안써도댐
-            presets: ['@babel/preset-env', '@babel/preset-react', '@babel/preset-typescript'],
-          },
-        },
-      },
       {
         test: /\.(ts|tsx)$/,
         loader: 'ts-loader',
@@ -42,8 +31,16 @@ module.exports = {
         // dev 모드이면 true로 설정하여 개발속도를 높이고, production 모드이면 d.ts 파일생성 및 타입 체크를 허용함으로써 안정성을 높일 수 있다.
         // 보통 속도향상과 안정성을 위해 transpile only는 true로 하고 fork-ts-checker-webpack-plugin라는 플러그인으로 타입 체크를 한다.
         options: {
-          transpileOnly: !isProd, // ❓
+          transpileOnly: !isProd, //
         },
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }, { loader: 'sass-loader' }],
+      },
+      {
+        test: /\.css$/i,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }],
       },
     ],
   },
@@ -61,6 +58,12 @@ module.exports = {
     open: true,
     hot: true,
     compress: true,
+    // 404를 표시하는 것 대신 다른 페이지를 노출
     historyApiFallback: true,
+  },
+  performance: {
+    hints: isProd && false,
+    maxEntrypointSize: 512000,
+    maxAssetSize: 512000,
   },
 };
