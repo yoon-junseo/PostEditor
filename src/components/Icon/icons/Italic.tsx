39e11e3a1af0fe6656df0b45930651a6f335fc8c
junzzi <jjh01027717502@gmail.com>
2021-10-05 18:56:06 +0900

    feat: Add Italic Icon

---
+++ src/components/Icon/icons/Italic.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Italic() {
+  return (
+    <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M19.9987 26.6666H9.33203V23.9999H13.2347L16.056 7.99992H11.9987V5.33325H22.6654V7.99992H18.7627L15.9414 23.9999H19.9987V26.6666Z"
+        fill="#444444"
+      />
+    </svg>
+  );
+}
+
+export default Italic;
