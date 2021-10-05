7c2bdfcd9bbc219dc3992558572afe3d9a2fb23d
junzzi <jjh01027717502@gmail.com>
2021-10-05 18:56:14 +0900

    feat: Add LeftAlign Icon

---
+++ src/components/Icon/icons/LeftAlign.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+
+function LeftAlign() {
+  return (
+    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M17 10H3"
+        stroke="#444444"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M21 6H3"
+        stroke="#444444"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M21 14H3"
+        stroke="#444444"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M17 18H3"
+        stroke="#444444"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default LeftAlign;
