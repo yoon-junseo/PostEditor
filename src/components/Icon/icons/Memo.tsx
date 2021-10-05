12420d2174e612032c2471502a5e1d13e3dc97c8
junzzi <jjh01027717502@gmail.com>
2021-10-05 18:56:27 +0900

    feat:Add Memo Icon

---
+++ src/components/Icon/icons/Memo.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+function Memo() {
+  return (
+    <svg width="24" height="29" viewBox="0 0 24 29" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M22.7373 20.2886L15.1133 27.7171V20.2886H22.7373Z"
+        stroke="#444444"
+        stroke-width="2.5"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M2.21094 2H22.7373V20.2857"
+        stroke="#444444"
+        stroke-width="2.5"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M15.1132 27.7143H2.21094V2"
+        stroke="#444444"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M7.34375 13L9.39638 17L17.0938 11"
+        stroke="#444444"
+        stroke-width="2.5"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default Memo;
