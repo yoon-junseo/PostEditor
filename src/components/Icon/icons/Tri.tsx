e58465548e84cf28e2b1c93b1307e3fd03c188d4
junzzi <jjh01027717502@gmail.com>
2021-10-12 14:53:57 +0900

    feat: Add Tri Icon

---
+++ src/components/Icon/icons/Tri.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Tri() {
+  return (
+    <svg width="13" height="9" viewBox="0 0 13 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M7.97617 7.38518C7.18321 8.25262 5.81679 8.25262 5.02383 7.38518L1.33459 3.34942C0.161335 2.06596 1.07185 -1.30506e-06 2.81076 -1.15304e-06L10.1892 -5.07993e-07C11.9281 -3.55973e-07 12.8387 2.06596 11.6654 3.34942L7.97617 7.38518Z"
+        fill="#4F4F4F"
+      />
+    </svg>
+  );
+}
+
+export default Tri;
