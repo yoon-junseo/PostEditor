1b8a78817ac7f6c38f18350cb57022c33b1d6370
junzzi <jjh01027717502@gmail.com>
2021-10-12 17:35:04 +0900

    feat: Add Styling in Editor

--- src/components/Editor/EditorScreen/index.tsx
+++ src/components/Editor/EditorScreen/index.tsx
@@ -1,11 +1,22 @@
 import { EditorScreenType } from '@/components/Editor/EditorScreen/types';
 import React from 'react';
+import styles from '@/containers/EditorPage/styles/editor.module.scss';
 function EditorScreen({ setValue }: EditorScreenType) {
   const onChange = (e: React.FormEvent<HTMLDivElement>) => {
     setValue((e.target as HTMLDivElement).innerHTML);
   };
   return (
-    <div style={{ marginTop: '100px' }} contentEditable onInput={onChange}>
+    <div
+      className={styles.editorScreenWrapper}
+      contentEditable
+      onInput={onChange}
+      onSelect={e => {
+        console.log(e);
+      }}
+      onTouchStart={e => {
+        console.log(e);
+      }}
+    >
       style
     </div>
   );
--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -10,6 +10,18 @@ function Editor({ value, setValue }: IEditorType) {
   return (
     <div className={styles.editorWrapper}>
       <EditorToolbar></EditorToolbar>
+      <div
+        style={{
+          marginTop: '100px',
+          fontSize: '32px',
+          display: 'flex',
+          justifyContent: 'center',
+          color: 'pink',
+          fontWeight: 'bolder',
+        }}
+      >
+        🏳️‍🌈 준찌의 에디터 세상에 온 걸 환영합니다. 🏳️‍🌈
+      </div>
       <EditorScreen setValue={setValue}></EditorScreen>
     </div>
   );
